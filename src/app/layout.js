f069b2f166a45f4d8fffc0a8f5438161088259b8
Junaid Faiz <he630001@gmail.com>
2025-04-21 23:23:44 +0500

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Transition from "../components/Transition";
+import Transition from "@/components/Transition";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {

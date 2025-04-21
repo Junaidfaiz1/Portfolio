04db601434dd12883526d0ebc493bb7faca190d6
Junaid Faiz <he630001@gmail.com>
2025-04-22 00:16:02 +0500

    fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 const Footer = () => {
     const currentYear = new Date().getFullYear(); // Get the current year
     return (
-        <footer className="flex justify-center items-center h-24 bg-gradient-to-b from-blue-500 to-purple-500 text-white shadow-inner">
+        <div className="flex justify-center items-center h-24 bg-gradient-to-b from-blue-500 to-purple-500 text-white shadow-inner">
             <p className="text-sm md:text-base font-medium">
                 © {currentYear} <span className="font-bold">Junaid Faiz</span>. All rights reserved.
             </p>
-        </footer>
+        </div>
     );
 };
 

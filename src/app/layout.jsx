f02a42116cc3667d2ccf4721c99faa2c2ade237b
Junaid Faiz <he630001@gmail.com>
2025-04-21 23:51:36 +0500

    fixing

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Transition from "@/components/Transition.jsx";
+import Transition from "@/components/Transition";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 "use client"
-import HeroSection from "@/components/HeroSection.jsx"
+import HeroSection from "@/components/HeroSection"
 import Skills from "@/app/about/components/Skills";
 import AboutMe from "@/app/about/components/AboutMe";
 import Projects from "@/app/projects/components/Projects";
 import ContactUs from "@/app/contact/components/contact-us";
-import Footer from "@/components/Footer.jsx";
-const Homepage = () => {
+import Footer from "@/components/Footer";
+const page = () => {
   return (
     <>
     <HeroSection/>
@@ -13,11 +13,9 @@ const Homepage = () => {
     <Skills/>
     <Projects/>
     <ContactUs/>
-    <Footer/>
-   
-    
+    <Footer/> 
    </>
   );
 };
 
-export default Homepage;
+export default page;
